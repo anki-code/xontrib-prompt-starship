8472c6d39935d792f5de59882c68cc2bc2ab7bc3
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-07 08:54:37 +0300

    Update prompt_starship.py

--- xontrib/prompt_starship.py
+++ xontrib/prompt_starship.py
@@ -1,6 +1,7 @@
+__xonsh__.env['STARSHIP_SHELL'] = 'sh'  # Fix https://github.com/anki-code/xontrib-prompt-starship/issues/1
 __xonsh__.env['PROMPT'] = lambda: __xonsh__.subproc_captured_stdout([
     'starship', 'prompt',
     '--status', str(__xonsh__.history[-1].rtn) if len(__xonsh__.history) > 0 else '0',
-    '--cmd-duration', str(__xonsh__.history[-1].ts[1] - __xonsh__.history[-1].ts[0]) if len(__xonsh__.history) > 0 else '0',
+    '--cmd-duration', str(int((__xonsh__.history[-1].ts[1] - __xonsh__.history[-1].ts[0])*1000)) if len(__xonsh__.history) > 0 else '0',
     '--jobs', str(len(__xonsh__.all_jobs))
 ])
