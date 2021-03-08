9dc8f63923a297d3ca10a7fada04b63f055e305c
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-08 10:18:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-prompt-starship',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
