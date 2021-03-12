a6002b61815e56365422840765025c5cff77abe5
eugenesvk <eugenesvk@users.noreply.github.com>
2021-03-12 15:28:42 +0300

    :bookmark: bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-prompt-starship',
-    version='0.0.5',
+    version='0.1.0',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
