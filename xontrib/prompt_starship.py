fb6c004cb21dc28ae8baf823561dcf00a36ad8aa
Jochen Schröder <1154946+cycomanic@users.noreply.github.com>
2022-12-21 21:33:05 +1300

    Make starship aware of terminal width
    fixes #8

--- xontrib/prompt_starship.py
+++ xontrib/prompt_starship.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from pathlib import Path
 
 
@@ -12,7 +13,8 @@ def _starship_prompt(cfg=None):
             'starship', 'prompt',
             '--status', str(int( __xonsh__.history[-1].rtn)) if len(__xonsh__.history) > 0 else '0',
             '--cmd-duration' , str(int((__xonsh__.history[-1].ts[1] - __xonsh__.history[-1].ts[0])*1000)) if len(__xonsh__.history) > 0 else '0',
-            '--jobs', str(len(__xonsh__.all_jobs))
+            '--jobs', str(len(__xonsh__.all_jobs)),
+            '--terminal-width', str(os.get_terminal_size().columns),
         ])
 
     
